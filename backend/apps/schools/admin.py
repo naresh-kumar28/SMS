b61081fe12101926da935c75eab10eababf81797
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-06-30 11:03:08 +0530

    create superuser and register School model in admin with display and search configurations

--- backend/apps/schools/admin.py
+++ backend/apps/schools/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import School
 
 # Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone_number', 'email')
+    search_fields = ('name', 'address', 'phone_number', 'email')
+    list_editable = ('is_active',)
+    prepopulated_fields = {'slug': ('name',)}
